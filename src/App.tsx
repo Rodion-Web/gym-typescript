56d84c29c1b1bfb091af210982cf393a11fabf09
Rodion-Web <rodeus770@gmail.com>
2025-03-13 01:36:24 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import OurClasses from "@/scenes/ourClasses";
 import Benefits from "@/scenes/benefits";
-import ContactUs from "@/scenes/contactUs"
-import Footer from "@/scenes/footer"
+import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -35,7 +35,7 @@ function App() {
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
       <ContactUs setSelectedPage={setSelectedPage} />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
