ec064da8e06058c928566341bfdb9dbd216ac77a
Rodion-Web <63527342+Rodion-Web@users.noreply.github.com>
2025-03-13 02:02:35 +0300

    Update index.tsx

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -15,6 +15,7 @@ type Props = {
 
 const Home = ({ setSelectedPage }: Props) => {
   const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/* IMAGE AND MAIN HEADER */}
@@ -37,16 +38,18 @@ const Home = ({ setSelectedPage }: Props) => {
             }}
           >
             <div className="relative">
-              <div className="before:absolute before:-left-20 before:-top-20 before:z-[-1] before:content-evolvetext">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                 <img alt="home-page-text" src={HomePageText} />
               </div>
             </div>
+
             <p className="mt-8 text-sm">
               Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
               Studios to get the Body Shapes That you Dream of.. Get Your Dream
               Body Now.
             </p>
           </motion.div>
+
           {/* ACTIONS */}
           <motion.div
             className="mt-8 flex items-center gap-8"
@@ -71,19 +74,24 @@ const Home = ({ setSelectedPage }: Props) => {
             </AnchorLink>
           </motion.div>
         </div>
+
         {/* IMAGE */}
-        <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
-          <img src={HomePageGraphic} alt="home-pageGraphic" />
+        <div
+          className="flex basis-3/5 justify-center md:z-10
+              md:ml-40 md:mt-16 md:justify-items-end"
+        >
+          <img alt="home-pageGraphic" src={HomePageGraphic} />
         </div>
       </motion.div>
+
       {/* SPONSORS */}
       {isAboveMediumScreens && (
         <div className="h-[150px] w-full bg-primary-100 py-10">
           <div className="mx-auto w-5/6">
             <div className="flex w-3/5 items-center justify-between gap-8">
-              <img src={SponsorRedBull} alt="redbull-sponsor" />
-              <img src={SponsorForbes} alt="forbes-sponsor" />
-              <img src={SponsorFortune} alt="fortune-sponsor" />
+              <img alt="redbull-sponsor" src={SponsorRedBull} />
+              <img alt="forbes-sponsor" src={SponsorForbes} />
+              <img alt="fortune-sponsor" src={SponsorFortune} />
             </div>
           </div>
         </div>
